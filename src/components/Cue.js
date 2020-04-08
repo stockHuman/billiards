89cd24f6b3cc78bb69ef89185d929bf836612834
Michael Hemingway <mpghemingway@gmail.com>
2020-04-08 19:16:10 -0400

    remove setref crom cue

--- src/components/Cue.js
+++ src/components/Cue.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Cue({ setRef = {}, cueRef = {}, position = [ 0, 0, 0 ] }) {
+export default function Cue({ cueRef = {}, position = [ 0, 0, 0 ] }) {
 	return (
-		<mesh ref={setRef} cueBallRef={cueRef} speed={0.6} position={position} castShadow>
+		<mesh cueBallRef={cueRef} speed={0.6} position={position} castShadow>
 			<cylinderGeometry attach="geometry" args={[ 0.1, 0.15, 12, 32, 32 ]} />
 			<meshPhongMaterial attach="material" color={0xffffff} />
 		</mesh>
